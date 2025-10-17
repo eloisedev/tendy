4bd59bafe6c8eda45f0d3d9fc3e11b06a3f56e52
eloise devito <eloisedevito@icloud.com>
2025-10-17 11:57:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the *public* folder
 app.use(express.static(path.join(process.cwd(), 'public')));
 
-// Serve index.html at the root route
 app.get('/', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
 });
 
-// API route to serve ice_times.json
 app.get('/api/ice-times', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'ice_times.json'));
 });
